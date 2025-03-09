efc23d0d5452defb66cfd21251467a7fb9983343
anthonyamar <anthonyamar@labodessons.fr>
2025-03-09 14:42:36 +0100

    finish to change name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "god_stack_production"
+  # config.active_job.queue_name_prefix = "geo_stack_production"
 
   config.action_mailer.perform_caching = false
 
