cf052dffbebb70a9ddaeb075aae5e872a2853a54
anthonyamar <anthonyamar@labodessons.fr>
2025-03-21 22:43:51 +0100

    add github CI and dependabot

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module GeoStack
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_support.to_time_preserves_timezone = :zone
 
     # I18n
     config.i18n.available_locales = %i[fr en]
